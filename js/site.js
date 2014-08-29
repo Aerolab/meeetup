16a751c5f8caaea5fa65b8e0697fa33d74ed3943
RobertoGonzalez <roberto@aerolab.com.ar>
2014-08-28 21:45:36 -0300

    The background is controlled with the accelerometer.

--- js/site.js
+++ js/site.js
@@ -1,6 +1,51 @@
-/*$(document).ready(function() {
+
+function updateBackground(offsetFromLeft) {
+
+  var totalWidth = window.innerWidth;
+  var minWidth = 1280;
+
+  offsetFromLeft *= 2;
+
+  // The background will be displaced at most by X% of the width
+  var displacementPercent = 1 / 100;
+  // This is a minimum, mostly for mobile devices
+  var minDisplacement = 15;
+
+  var maxDisplacement = displacementPercent * totalWidth;
+  maxDisplacement = (maxDisplacement < minDisplacement) ? minDisplacement : maxDisplacement;
+
+  var additionalOffset = (totalWidth < minWidth) ? (minWidth - totalWidth) / 2.0 - maxDisplacement : 0.0;
+
+  $('#hero').css('background-position', (- offsetFromLeft * maxDisplacement - additionalOffset) + 'px ' + '100%');
+
+}
+
+
+$(document).ready(function() {
 	$('#hero').mousemove(function(e){
-    var amountMovedX = (e.pageX * -1 / 10);
-    $(this).css('background-position', amountMovedX + 'px ' + 'bottom');
+
+    // Calculate where the mouse is with respect to the center of the screen (0.0 to 1.0)
+    var totalWidth = window.innerWidth;
+    var offset = e.pageX / totalWidth;
+
+    updateBackground( offset );
+
   });
-});*/
+
+  $(window).resize(function(){
+    //$('#hero')
+  });
+
+  window.addEventListener('deviceorientation', function(event){
+
+    var maxRoll = 15;
+    // roll is left-to-right inclination
+    roll = event.gamma;
+    roll = (roll > maxRoll) ? maxRoll : roll;
+    roll = (roll < -maxRoll) ? -maxRoll : roll;
+
+    var offset = (roll + maxRoll) / maxRoll / 2.0;
+
+    updateBackground( offset );
+  });
+});
