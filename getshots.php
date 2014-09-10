c9ce9a93648b9ed620adfa09546831307908d12e
Patricio Pitaluga <patricio.pitaluga@gmail.com>
2014-09-10 17:33:44 -0300

    Some fixes

--- getshots.php
+++ getshots.php
@@ -1,29 +1,30 @@
-<?	require_once 'src/dribbble/api/client.php';
-		$client = new Client();
+<?php
+	require_once 'src/dribbble/api/client.php';
+	$client = new Client();
 
-		$ar_shots = Array();
+	$ar_shots = Array();
 
-		if (1 == 1) {
-			$shots = $client->getPlayerShots('aerolab', 1, 3);
-			$first_shots = array_slice($shots->shots, 0, 3);
-
-			$ar_positions = Array(0, 2, 4);
-			$count = 0;
-			foreach ($first_shots as $shot) {
-				$ar_shots[$ar_positions[$count]] = '<div><a href="'.$shot->url.'"><img src="'.$shot->image_url.'" alt="'.$shot->title.'"/></a></div>';
-				$count++;
-			}
-		}
-
-		$shots = $client->getPlayerShots('indicius', 1, 3);
+	if (1 == 1) {
+		$shots = $client->getPlayerShots('aerolab', 1, 3);
 		$first_shots = array_slice($shots->shots, 0, 3);
 
-		$ar_positions = Array(1, 3, 5);
+		$ar_positions = Array(0, 2, 4);
 		$count = 0;
 		foreach ($first_shots as $shot) {
-			$ar_shots[$ar_positions[$count]] = '<div><a href="'.$shot->url.'"><img src="'.$shot->image_url.'" alt="'.$shot->title.'"/></a></div>';
+			$ar_shots[$ar_positions[$count]] = '<div><img src="'.$shot->image_url.'" alt="'.$shot->title.'"/><a href="'.$shot->url.'" class="details"><span>'.$shot->title.'</span></a></div>';
 			$count++;
 		}
+	}
 
-		echo implode("\n", $ar_shots);
+	$shots = $client->getPlayerShots('indicius', 1, 3);
+	$first_shots = array_slice($shots->shots, 0, 3);
+
+	$ar_positions = Array(1, 3, 5);
+	$count = 0;
+	foreach ($first_shots as $shot) {
+		$ar_shots[$ar_positions[$count]] = '<div><img src="'.$shot->image_url.'" alt="'.$shot->title.'"/><a href="'.$shot->url.'" class="details"><span>'.$shot->title.'</span></a></div>';
+		$count++;
+	}
+
+	echo implode("\n", $ar_shots);
 
